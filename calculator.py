b3671f4def5d123ecf47c0f7abebeff4703806aa
Bud Welsh <budwelsh14@gmail.com>
2020-01-05 07:01:32 -0600

    Add player variables to file

---
+++ calculator.py
@@ -0,0 +1,17 @@
+player_name = "Babe Ruth"
+games = 2503
+at_bats = 8398
+plate_apperances = 10616
+singles = 1517
+doubles = 506
+triples = 136
+homeruns = 714
+runs = 2174
+runs_batted_in = 2217
+walks = 2062
+strikeouts = 1330
+hit_by_pitch = 43
+sacrifice_hit = 113
+stolen_bases = 123
+caught_stealing = 117
+
