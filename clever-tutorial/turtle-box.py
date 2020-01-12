e2481a4c7324ed7e55f7d73b1e08ab7e37c85661
Bud Welsh <budwelsh14@gmail.com>
2020-01-12 11:19:14 -0600

    Make a box with turtle

---
+++ clever-tutorial/turtle-box.py
@@ -0,0 +1,10 @@
+import turtle
+
+bud_turtle = turtle.Turtle()
+bud_turtle.forward(100)
+bud_turtle.right(90)
+bud_turtle.forward(100)
+bud_turtle.right(90)
+bud_turtle.forward(100)
+bud_turtle.right(90)
+bud_turtle.forward(100)
