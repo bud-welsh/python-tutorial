586846b9191e10905e14b19af1d46bd1798d4116
Bud Welsh <budwelsh14@gmail.com>
2020-01-12 11:32:30 -0600

    Add square function file

---
+++ clever-tutorial/function-tutorial.py
@@ -0,0 +1,17 @@
+import turtle
+
+bud_turtle = turtle.Turtle()
+
+# This is a square
+def square():
+    bud_turtle.forward(100)
+    bud_turtle.right(90)
+    bud_turtle.forward(100)
+    bud_turtle.right(90)
+    bud_turtle.forward(100)
+    bud_turtle.right(90)
+    bud_turtle.forward(100)
+
+square()
+bud_turtle.forward(100)
+square()
